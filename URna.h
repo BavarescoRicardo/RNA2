8d1dca7538e8fb7e1490b1634c871bfa522f2547
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-02-26 17:47:51 -0300

    alterado incluido leitura de arquivo e mudado nome do projeto
    Leitura copiada do projeto que fiz com o igor a rede neural de imagens.
    Nome do projeto mudado para SistemaIntelignt

--- URna.h
+++ URna.h
@@ -58,13 +58,15 @@ class TFmRna : public TForm
 	TListBox *ListBox2;
 	TFastLineSeries *Series3;
 	TFastLineSeries *Series4;
+	TPanel *Panel1;
+	TButton *Button3;
 		void __fastcall Button1Click(TObject *Sender);
 		void __fastcall Button2Click(TObject *Sender);
-        void lerArquivos(AnsiString ArquivoDeEventos);
         void __fastcall AtualizaGrafico();
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall ListBox1Click(TObject *Sender);
 	void __fastcall ListBox2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
 
 
 	private:	// User declarations
